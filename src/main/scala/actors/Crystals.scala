6492a45b3eb6980c3390c072f2d06f5f095461d6
Mark Lewis <mlewis@trinity.edu>
2019-09-27 10:59:22 -0500

    Finished the crystal growth with actors.

--- src/main/scala/actors/Crystals.scala
+++ src/main/scala/actors/Crystals.scala
@@ -1,13 +1,18 @@
 package actors
+
+import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.image.WritableImage
 import scalafx.scene.image.ImageView
 import akka.actor.ActorSystem
+import akka.actor.Props
+import scalafx.stage.WindowEvent
 
 object Crystals extends JFXApp {
   val system = ActorSystem("Crystals")
   val img = new WritableImage(800, 800)
+  val manager = system.actorOf(Props(new CrystalManager(img)))
 
 
   stage = new JFXApp.PrimaryStage {
@@ -16,5 +21,6 @@ object Crystals extends JFXApp {
       val view = new ImageView(img)
       content = view
     }
+    onCloseRequest = (we: WindowEvent) => system.terminate()
   }
 }
