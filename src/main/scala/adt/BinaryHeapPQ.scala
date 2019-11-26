d3a791e4d37a5d5dc853af8a21b520e415ba0859
Mark Lewis <mlewis@trinity.edu>
2019-11-26 11:10:42 -0600

    Added binary heap.

---
+++ src/main/scala/adt/BinaryHeapPQ.scala
@@ -0,0 +1,41 @@
+package adt
+import scala.reflect.ClassTag
+
+class BinaryHeapPQ[A : ClassTag](higherP: (A, A) => Boolean) extends MyPriorityQueue[A] {
+  private var heap = Array.fill(10)(null.asInstanceOf[A]) // new Array[A](10)
+  private var back = 1
+
+  def dequeue(): A = {
+    val ret = heap(1)
+    back -= 1
+    var stone = 1
+    var flag = true
+    while (stone * 2 < back && flag) {
+      var betterChild = stone * 2
+      if (stone * 2 + 1 < back && higherP(heap(stone * 2 + 1), heap(betterChild))) betterChild += 1
+      if (higherP(heap(betterChild), heap(back))) {
+        heap(stone) = heap(betterChild)
+        stone = betterChild
+      } else flag = false
+    }
+    heap(stone) = heap(back)
+    ret
+  }
+
+  def enqueue(value: A): Unit = {
+    if (back >= heap.length) {
+      heap = Array.tabulate(heap.length * 2)(i => if (i < heap.length) heap(i) else null.asInstanceOf[A])
+    }
+    var bubble = back
+    while (bubble > 1 && higherP(value, heap(bubble / 2))) {
+      heap(bubble) = heap(bubble / 2)
+      bubble /= 2
+    }
+    heap(bubble) = value
+    back += 1
+  }
+
+  def peek(): A = heap(1)
+
+  def isEmpty: Boolean = back == 1
+}
--- src/test/scala/adt/TestPriorityQueue.scala
+++ src/test/scala/adt/TestPriorityQueue.scala
@@ -9,6 +9,34 @@ class TestPriorityQueue {
 
   @Before
   def makePQ(): Unit = {
-    pq = new UnsortedArrayPQ[Int](_ < _)
+    pq = new BinaryHeapPQ[Int](_ < _)
+  }
+
+  @Test
+  def emptyOnCreate(): Unit = {
+    assertTrue(pq.isEmpty)
+  }
+
+  @Test
+  def addRemoveThree(): Unit = {
+    pq.enqueue(8)
+    pq.enqueue(2)
+    pq.enqueue(4)
+    assertEquals(2, pq.peek())
+    assertEquals(2, pq.dequeue())
+    assertEquals(4, pq.peek())
+    assertEquals(4, pq.dequeue())
+    assertEquals(8, pq.peek())
+    assertEquals(8, pq.dequeue())
+  }
+
+  @Test
+  def bigTest(): Unit = {
+    val nums = Array.fill(10000000)(util.Random.nextInt)
+    for (n <- nums) pq.enqueue(n)
+    for (n <- nums.sorted) {
+      assertEquals(n, pq.peek())
+      assertEquals(n, pq.dequeue())
+    }
   }
 }
